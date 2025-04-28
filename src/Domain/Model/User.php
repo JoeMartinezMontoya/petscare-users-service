4b27cd1a263b72f5694d21b188442c3a96101efe
Joë <martinezmontoyajoe@gmail.com>
2025-04-28 12:11:34 +0200

    feat(app): model entity

--- src/Domain/Model/User.php
+++ src/Domain/Model/User.php
@@ -1,7 +1,39 @@
 <?php
 namespace App\Domain\Model;
 
+use App\Domain\ValueObjects\BirthDate;
+use App\Domain\ValueObjects\CreatedAt;
+use App\Domain\ValueObjects\FirstName;
+use App\Domain\ValueObjects\Id;
+use App\Domain\ValueObjects\LastName;
+use App\Domain\ValueObjects\UserName;
+
 class User
 {
+    public function __construct(
+        private Id $id,
+        private UserName $userName,
+        private FirstName $firstName,
+        private LastName $lastName,
+        private BirthDate $birthDate,
+        private CreatedAt $createdAt,
+    ) {}
 
+    public function getId(): Id
+    {return $this->id;}
+
+    public function getUserName(): UserName
+    {return $this->userName;}
+
+    public function getFirstName(): FirstName
+    {return $this->firstName;}
+
+    public function getLastName(): LastName
+    {return $this->lastName;}
+
+    public function getBirthDate(): BirthDate
+    {return $this->birthDate;}
+
+    public function getCreatedAt(): CreatedAt
+    {return $this->createdAt;}
 }
