b561257262574714c196c802e14795937125e707
Joë <martinezmontoyajoe@gmail.com>
2025-04-28 21:13:28 +0200

    feat(app): create user usecase
    Creation responsability separation

--- src/Application/UseCase/CreateUser.php
+++ src/Application/UseCase/CreateUser.php
@@ -9,19 +9,46 @@ use App\Domain\ValueObjects\FirstName;
 use App\Domain\ValueObjects\Id;
 use App\Domain\ValueObjects\LastName;
 use App\Domain\ValueObjects\UserName;
+use App\Infrastructure\Exception\ApiException;
 use App\Infrastructure\Service\UuidGenerator;
+use App\Infrastructure\Utils\HttpStatusCodes;
+use Symfony\Component\DependencyInjection\ParameterBag\ParameterBagInterface;
+use Symfony\Contracts\HttpClient\HttpClientInterface;
 
 class CreateUser
 {
-    public function __construct(private UserRepositoryInterface $repository)
+    public function __construct(private UserRepositoryInterface $repository, private HttpClientInterface $httpClient, private ParameterBagInterface $params)
     {}
 
     public function execute(array $data): bool
     {
-        // Générer l'UUID, récupérer mail et password et l'envoyer au Auth service
-        $uuid = (new UuidGenerator)->generate();
+        if (! isset($data['email']) || ! isset($data['password'])) {
+            throw new ApiException(
+                'https://api.petscare.com/error/invalid-data',
+                'Invalid Data',
+                'User creation impossible without credentials.',
+                HttpStatusCodes::BAD_REQUEST,
+                null,
+                '/public/api/registration'
+            );
+        }
 
-        // Une fois la réponse de auth recu on continue ici
+        $uuid     = (new UuidGenerator)->generate();
+        $authData = ['uuid' => $uuid, 'email' => $data['email'], 'password' => $data['password']];
+
+        try {
+            $authServiceUrl = $this->params->get('auth_service_url');
+
+            $response = $this->httpClient->request(method: 'POST', url: (string) $authServiceUrl . '/public/api/registration', options: ['json' => json_encode($authData)]);
+            $response = json_decode($response->getContent(), true);
+        } catch (\Exception $e) {
+            throw new ApiException(
+                "https://api.petscare.com/errors/failed-registration",
+                "Failed Registration",
+                $e->getMessage(),
+                HttpStatusCodes::BAD_REQUEST
+            );
+        }
         $user = new User(
             new Id($uuid),
             new UserName($data['userName']),
--- src/Tests/Application/UseCase/CreateUserTest.php
+++ src/Tests/Application/UseCase/CreateUserTest.php
@@ -3,8 +3,11 @@ namespace App\Tests\Application\UseCase;
 
 use App\Application\UseCase\CreateUser;
 use App\Domain\Repository\UserRepositoryInterface;
+use App\Infrastructure\Exception\ApiException;
 use PHPUnit\Framework\Attributes\Group;
 use PHPUnit\Framework\TestCase;
+use Symfony\Component\DependencyInjection\ParameterBag\ParameterBagInterface;
+use Symfony\Contracts\HttpClient\HttpClientInterface;
 
 #[Group('unit')]
 #[Group('usecase')]
@@ -15,7 +18,34 @@ final class CreateUserTest extends TestCase
         $repository = $this->createMock(UserRepositoryInterface::class);
         $repository->expects($this->once())->method('save')->willReturn(true);
 
-        $useCase = new CreateUser($repository);
+        $httpClient = $this->createMock(HttpClientInterface::class);
+
+        $params = $this->createMock(ParameterBagInterface::class);
+        $params->expects($this->once())->method('get')->willReturn('http://auth-service');
+
+        $useCase = new CreateUser($repository, $httpClient, $params);
+
+        $data = [
+            'userName'  => 'EddieVH',
+            'lastName'  => 'Van Halen',
+            'firstName' => 'Eddie',
+            'birthDate' => '1993-11-05',
+            'email'     => 'admin@test.fr',
+            'password'  => 'testpassword',
+        ];
+
+        $result = $useCase->execute($data);
+        $this->assertTrue($result);
+    }
+
+    public function testMustReturnAnErrorIfInvalidDataProvided()
+    {
+
+        $repository = $this->createMock(UserRepositoryInterface::class);
+        $httpClient = $this->createMock(HttpClientInterface::class);
+        $params     = $this->createMock(ParameterBagInterface::class);
+
+        $useCase = new CreateUser($repository, $httpClient, $params);
 
         $data = [
             'userName'  => 'EddieVH',
@@ -24,7 +54,7 @@ final class CreateUserTest extends TestCase
             'birthDate' => '1993-11-05',
         ];
 
-        $result = $useCase->execute($data);
-        $this->assertTrue($result);
+        $this->expectException(ApiException::class);
+        $useCase->execute($data);
     }
 }
